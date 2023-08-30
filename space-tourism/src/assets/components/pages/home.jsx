0a2d4ce9edd143c39e9cfd19e10f1c5accaa78cf
Loic Lion <loic.lion.contact@gmail.com>
2023-08-30 18:00:04 +0200

    suite scss

--- space-tourism/src/assets/components/pages/home.jsx
+++ space-tourism/src/assets/components/pages/home.jsx
@@ -1,16 +1,20 @@
+import "../../css/home.css";
+
 export default function Home() {
   return (
     <>
-      <section>
-        So, you want to travel to Space Let’s face it; if you want to go to
-        space, you might as well genuinely go to outer space and not hover kind
-        of on the edge of it. Well sit back, and relax because we’ll give you a
-        truly out of this world experience!
+      <section className="container__home">
+        <h5>SO, YOU WANT TO TRAVEL TO</h5>
+        <h1>SPACE</h1>
+        <p>
+          Let’s face it; if you want to go to space, you might as well genuinely
+          go to outer space and not hover kind of on the edge of it. Well sit
+          back, and relax because we’ll give you a truly out of this world
+          experience!
+        </p>
       </section>
-      <section>
-        <div>
-          <span>Explore</span>
-        </div>
+      <section className="container__explore">
+        <div>EXPLORE</div>
       </section>
     </>
   );
