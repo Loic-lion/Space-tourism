028c0ffa9d5b0551d211f0a79ef71083c06a29eb
Loic Lion <loic.lion.contact@gmail.com>
2023-09-05 17:40:38 +0200

    js bar nav destination

--- space-tourism/src/assets/components/pages/destination/navbar-destination.jsx
+++ space-tourism/src/assets/components/pages/destination/navbar-destination.jsx
@@ -1,14 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 
 export default function NavBarDestination({ planet }) {
+  const [activeLink, setActiveLink] = useState(0); // Initialisation avec l'index du premier lien
+
+  const handleLinkClick = (index) => {
+    setActiveLink(index);
+  };
+
   return (
     <>
       <nav className="cont__destination__navigation__bar">
         <ul>
           {planet.map((destination, index) => (
             <li key={index}>
-              <Link  to={destination.name}>
+              <Link
+                to={destination.name}
+                onClick={() => handleLinkClick(index)}
+                style={{
+                  color: activeLink === index ? "rgba(255, 255, 255)" : "rgba(255, 255, 255, 0.5)", // Change the color as needed
+                }}
+              >
                 {destination.name}
               </Link>
             </li>
